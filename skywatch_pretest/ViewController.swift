85eeab75a1ad7aae679171436df5a54b72d71b63
John Chang <john107205@weexglobal.com>
2023-07-22 10:38:39 +0800

    Initial Commit

---
+++ skywatch_pretest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  skywatch_pretest
+//
+//  Created by liyuan chang on 2023/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
